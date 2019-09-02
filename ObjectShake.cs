03eaa5bcf0437974df0ed23c2d1f2e1b7cd79b7d
khiemnd777 <khiemnd777@gmail.com>
2019-09-02 21:23:54 +0700

    Add files via upload

---
+++ ObjectShake.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectShake : MonoBehaviour
+{
+    public Transform shakedObject;
+    public float duration;
+    public float magnitude;
+    Vector3 _originalLocalPosition;
+
+    void Start ()
+    {
+        _originalLocalPosition = shakedObject.localPosition;
+    }
+
+    public void Shake ()
+    {
+        Shake (duration, magnitude);
+    }
+
+    public void Shake (float duration, float magnitude)
+    {
+        StartCoroutine (Shaking (duration, magnitude));
+    }
+
+    public IEnumerator Shaking (float duration, float magnitude)
+    {
+        var elapsed = 0f;
+        while (elapsed <= 1f)
+        {
+            shakedObject.localPosition = _originalLocalPosition + Random.insideUnitSphere * magnitude;
+            elapsed += Time.fixedDeltaTime / duration;
+            yield return new WaitForFixedUpdate ();
+        }
+        shakedObject.localPosition = _originalLocalPosition;
+    }
+}
